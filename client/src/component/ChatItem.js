e08ff43d480475a93659246f6540a6f22e8ef894
steve1994 <steve.harnadi@gmail.com>
2019-10-22 16:55:22 +0700

    Move all bin/www server to app.js

--- client/src/component/ChatItem.js
+++ client/src/component/ChatItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import Remarkable from 'remarkable';
-// import RemarkableReactRenderer from 'remarkable-react';
+const ReactMarkdown = require('react-markdown');
 
 export default class ChatItem extends React.Component {
 
@@ -14,12 +13,6 @@ export default class ChatItem extends React.Component {
         this.props.onDelete(this.props.id);
     }
 
-    getRawMarkUp() {
-        // const md = new Remarkable();
-        // md.renderer = new RemarkableReactRenderer();
-        // return {__html : md.render(this.props.name)};
-    }
-
     resend() {
         this.props.resendMethod(this.props.id,this.props.name,this.props.text);
     }
@@ -31,8 +24,8 @@ export default class ChatItem extends React.Component {
                     <img class="media-object" data-src="holder.js/64x64" alt="64x64" style={{width: "32px", height:"32px"}} src="red-minus-sign.png" />
                 </div>
                 <div class="media-body">
-                    <h5 class="media-heading" /*dangerouslySetInnerHTML={this.getRawMarkUp()}*/>{this.props.name}</h5>
-                    <small class="col-lg-12">{this.props.text}</small>
+                    <h5 class="media-heading">{this.props.name}</h5>
+                    <small class="col-lg-12"><ReactMarkdown source={this.props.text} /></small>
                 </div>
                 {this.props.isResend ? <button class="col-lg-12" onClick={this.resend}>Resend</button>  : ''}
 
